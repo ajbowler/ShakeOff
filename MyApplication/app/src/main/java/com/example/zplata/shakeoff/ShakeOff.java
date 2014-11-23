a841ff4ae76d4df725020e568891b1f5d5aba1df
rmquinn <rmquinn@iastate.edu>
2014-11-23 00:09:03 -0600

    working on boss logic

--- MyApplication/app/src/main/java/com/example/zplata/shakeoff/ShakeOff.java
+++ MyApplication/app/src/main/java/com/example/zplata/shakeoff/ShakeOff.java
@@ -59,21 +59,23 @@ public class ShakeOff extends Activity {
     //Boss Values
     private boolean bossFight; // is the boss fight happening
     private int tempShakes = 0;
-
+    private int image = 0;
+    private int kShakes;
+    private int bossTime; // milliseconds
+    private int maxTime = 15000; // 15 seconds to defeat Nick
     private boolean youWin = false;
 
     //Venmo Auth
     private String auth = "padD3bRMsJtbePcZ3QKd6V3WxYXs8EPa";
     private String amt = "0.01";
 
-    private int image = 0;
+
 
 
 
     // Timer
     // timerHandler.postDelayed(timerRunnable, 0); to start
-    // timerHandler.removeCallbacks(timerRunnable); to pause
-    long startTime;
+    // timerHandler.removeCallbacks(timerRunnable); to pause (not necessary)
     Handler timerHandler = new Handler();
     Runnable timerRunnable = new Runnable() {
 
@@ -98,12 +100,23 @@ public class ShakeOff extends Activity {
                     hiddenBossImg3.setVisibility(View.VISIBLE);
                 image++;
                 image%=4;
+
+                if(bossTime >= maxTime) {
+
+                    if(tempShakes >= kShakes) {
+                        centerCount.setText("LEVEL UP");
+                        centerCount.setVisibility(View.VISIBLE);
+                        level++;
+                        levelCount.setText("Level " + level);
+                        hiddenBossMsg.setVisibility(View.GONE);
+                    }
+                    bossFight = false;
+                }
             }
 
             timerHandler.postDelayed(this, 100); // calls itself in 10
         }
     };
-    // end Timer
 
 
     @Override
@@ -152,13 +165,7 @@ public class ShakeOff extends Activity {
             }
 
             public void handleShakeEvent() {
-                if(level % 5 == 0 && level != 0) {
-                    bossShake();
-                    bossFight = true;
-                }
-                else{
-                    shake();
-                }
+                shake();
             }
 
         });
@@ -166,37 +173,41 @@ public class ShakeOff extends Activity {
 
 
     public void shake() {
-        shakes++;
+
+        if(level % 5 == 0 && level != 0) {
+            bossShake();
+            bossFight = true;
+        }
+
+        if(!bossFight) { // because shakes is for leveling
+            shakes++;
+            levelProgressBar.incrementProgressBy(1);
+            if(shakes >= level * levelRequirement) {
+                shakes = 0;
+                centerCount.setText("LEVEL UP"); //TODO make a different text for this
+                level++;
+                levelCount.setText("Level " + level);
+                updateProgressBar();
+            }
+        }
+        else {
+            tempShakes++;
+        }
         totalShakes++;
-        levelProgressBar.incrementProgressBy(1);
         totalCount.setText("Total " + totalShakes);
         centerCount.setText(shakes + "");
-        if(shakes >= level * levelRequirement) {
-            shakes = 0;
-            centerCount.setText("LEVEL UP"); //TODO make a different text for this
-            level++;
-            levelCount.setText("Level " + level);
-            updateProgressBar();
-        }
     }
 
     private void bossShake () {
-        centerCount.setVisibility(View.GONE);
 
+        tempShakes = 0;
+        bossTime = 0;
+        kShakes = level * levelRequirement;
+
+        centerCount.setVisibility(View.GONE);
         hiddenBossMsg.setText("ShakeOff w/ Nicholas!");
         hiddenBossMsg.setVisibility(View.VISIBLE);
-        tempShakes++;
-        totalShakes++;
-        totalCount.setText("Total " + totalShakes);
-        int kShakes = level * levelRequirement;
-        if(tempShakes >= kShakes) {
-            tempShakes = 0;
-            centerCount.setText("LEVEL UP");
-            centerCount.setVisibility(View.VISIBLE);
-            level++;
-            levelCount.setText("Level " + level);
-            hiddenBossMsg.setVisibility(View.GONE);
-        }
+
         /*else{
             boolean venmoInstalled = VenmoLibrary.isVenmoInstalled(this);
             if(venmoInstalled){
